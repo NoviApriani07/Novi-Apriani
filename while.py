111dbfc0b127f42bf98c17bd66d4e17f9bbac764
Novi Apriani <100309119+NoviApriani07@users.noreply.github.com>
2022-04-22 17:12:14 +0700

    update

---
+++ while.py
@@ -0,0 +1,8 @@
+# Take user input
+number = 2  
+
+# Condition of the while loop
+while number < 5 :  
+    print("Thank you")
+    # Increment the value of the variable "number by 1"
+    number = number+1
